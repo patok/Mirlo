ae98ad425b2c459ed0681a1d9b3e68b0f0502599
Patricio Keilty <patricio.keilty@gmail.com>
2014-10-15 17:40:13 -0300

    Added an Status Adapter.

--- src/ar/edu/ips/aus/android/HomeActivity.java
+++ src/ar/edu/ips/aus/android/HomeActivity.java
@@ -1,6 +1,5 @@
 package ar.edu.ips.aus.android;
 
-import java.util.LinkedList;
 import java.util.List;
 
 import twitter4j.Status;
@@ -9,8 +8,12 @@ import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import ar.edu.ips.android.R;
 import ar.edu.ips.aus.android.TestTwitter4jLib.TwitterTest;
 
@@ -48,17 +51,47 @@ public class HomeActivity extends Activity {
 		protected void onPostExecute(List<twitter4j.Status> result) {
 			super.onPostExecute(result);
 
-			List<String> data = new LinkedList<String>();
-			for (twitter4j.Status status : result) {
-				data.add(status.getUser().getScreenName() + " :: "
-						+ status.getText());
-			}
-
 			ListView listView = (ListView) findViewById(R.id.listView1);
-			ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-					HomeActivity.this, R.layout.list_item_layout, data);
+			StatusAdapter adapter = new StatusAdapter(result);
 
 			listView.setAdapter(adapter);
+		}
+
+	}
+
+	class StatusAdapter extends BaseAdapter implements ListAdapter {
+
+		private final List<Status> data;
+
+		public StatusAdapter(List<Status> data) {
+			this.data = data;
+		}
+
+		@Override
+		public int getCount() {
+			return data.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return data.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return data.get(position).getId();
+		}
+
+		@Override
+		public View getView(int position, View reusableView,
+				ViewGroup parentView) {
+			if (reusableView == null) {
+				reusableView = getLayoutInflater().inflate(
+						R.layout.list_item_layout, null);
+			}
+			TextView textView = (TextView) reusableView;
+			textView.setText(data.get(position).getText());
+			return textView;
 		}
 
 	}
