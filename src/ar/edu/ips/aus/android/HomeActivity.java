1944a136e71c07fdc22582afb7f7f859936135a4
Patricio Keilty <patricio.keilty@gmail.com>
2014-10-15 15:54:27 -0300

    AsyncTask for retrieving tweets from home timeline.

--- src/ar/edu/ips/aus/android/HomeActivity.java
+++ src/ar/edu/ips/aus/android/HomeActivity.java
@@ -1,7 +1,11 @@
 package ar.edu.ips.aus.android;
 
+import twitter4j.TwitterException;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 import ar.edu.ips.android.R;
 import ar.edu.ips.aus.android.TestTwitter4jLib.TwitterTest;
 
@@ -14,7 +18,34 @@ public class HomeActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
 		
+		twitterTest = new TestTwitter4jLib.TwitterTest();
+		twitterTest.init();
+		
+		new RetrieveTweets().execute();
 	}
 	
+	class RetrieveTweets extends AsyncTask<Void, Void, String> {
+
+		private static final String TAG = "TwitterTests";
+
+		@Override
+		protected String doInBackground(Void... params) {
+	        try {
+				return twitterTest.buildHomeTimelineOutput();
+			} catch (TwitterException e) {
+				Log.e(TAG, e.getErrorMessage());
+				return e.getErrorMessage();
+			}
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			
+	        TextView textView = (TextView) findViewById(R.id.homeText);
+	        textView.setText(result);
+		}
+
+	}
 
 }
